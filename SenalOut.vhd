79613087a9f449bc1ca125088f07c3810db8358e
Manuel <manuelgop@gmail.com>
2013-11-22 11:45:08 -0600

    Agregué el Clk de 1MHz

---
+++ SenalOut.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    11:40:50 11/22/2013 
+-- Design Name: 
+-- Module Name:    SenalOut - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity SenalOut is
+end SenalOut;
+
+architecture Behavioral of SenalOut is
+
+begin
+
+
+end Behavioral;
+
